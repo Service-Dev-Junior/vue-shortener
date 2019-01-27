c4d3d2208a9ca3ab6912bfe3312a06776d635fc8
jwooss <wodn4131@gmail.com>
2019-01-27 23:30:20 +0900

    코드 정리

--- server/src/routes.php
+++ server/src/routes.php
@@ -3,25 +3,19 @@
 use Slim\Http\Request;
 use Slim\Http\Response;
 
-$app->options('/{routes:.+}', function ($request, $response, $args) {
-    return $response;
+// 저장된 리스트 모두 조회
+$app->get('/shorteners', function (Request $request, Response $response, array $args) {
+
 });
 
-$app->add(function ($req, $res, $next) {
-    $response = $next($req, $res);
-    return $response
-        ->withHeader('Access-Control-Allow-Origin', 'http://localhost:8080')
-        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-});
-
-$app->post('/shorten', function (Request $request, Response $response, array $args) {
+// URL 변환 및 저장
+$app->post('/shortener', function (Request $request, Response $response, array $args) {
     try {
-        $client   = new \GuzzleHttp\Client();
+        $client    = new \GuzzleHttp\Client();
         $_response = $client->request('GET', 'https://api-ssl.bitly.com/v3/shorten', [
             'query'  => [
                 'longUrl'      => 'https://frend.tistory.com/12?category=711253',
-                'access_token' => 'b65287033f465626a66db5fd87984d8ca17a36d0',
+                'access_token' => BITLY_ACCESS_TOKEN,
             ],
             'verify' => false,
         ]);
@@ -29,9 +23,19 @@ $app->post('/shorten', function (Request $request, Response $response, array $ar
         if ($_response->getStatusCode() == 200) {
             $body     = $_response->getBody();
             $arr_body = json_decode($body);
-            return $response->withJson(['data' => $arr_body->data->url]);
+
+            return $response->withJson([
+                'data' => [
+                    'short' => $arr_body->data->url
+                ]
+            ]);
         }
     } catch (Exception $e) {
         $this->logger->info($e->getMessage());
     }
+});
+
+// 삭제
+$app->delete('/shortener/{}', function (Request $request, Response $response, array $args) {
+
 });
--- server/src/settings.php
+++ server/src/settings.php
@@ -1,19 +1,27 @@
 <?php
 return [
     'settings' => [
-        'displayErrorDetails' => true, // set to false in production
+        'displayErrorDetails'    => true, // set to false in production
         'addContentLengthHeader' => false, // Allow the web server to send the content-length header
 
         // Renderer settings
-        'renderer' => [
+        'renderer'               => [
             'template_path' => __DIR__ . '/../templates/',
         ],
 
         // Monolog settings
-        'logger' => [
-            'name' => 'slim-app',
-            'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
+        'logger'                 => [
+            'name'  => 'slim-app',
+            'path'  => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
             'level' => \Monolog\Logger::DEBUG,
+        ],
+
+        // mysql 셋팅
+        "db"                     => [
+            "host"   => "localhost",
+            "dbname" => "wodn4131",
+            "user"   => "wodn4131",
+            "pass"   => "wogud00."
         ],
     ],
 ];
