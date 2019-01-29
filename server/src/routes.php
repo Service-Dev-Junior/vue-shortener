d9dc1879704011e04b88f37b6fa29294cfb99c5a
jwooss <wodn4131@gmail.com>
2019-01-30 01:24:21 +0900

    API 개발

--- server/src/routes.php
+++ server/src/routes.php
@@ -5,38 +5,94 @@ use Slim\Http\Response;
 
 // 저장된 리스트 모두 조회
 $app->get('/shorteners', function (Request $request, Response $response, array $args) {
+    try {
+        $_sth = $this->db->prepare("SELECT * FROM shortener ORDER BY id DESC");
+        $_sth->execute();
+        $_results = $_sth->fetchAll();
 
-    return $response->withStatus(300)->withJson(['test' =>'test']);
+        return $response->withJson([
+            'result' => RESPONSE_SUCCESS,
+            'data' => [
+                'list' => $_results
+            ],
+        ]);
+
+    } catch (Exception $e) {
+        $this->logger->error($e->getMessage());
+        return $response->withJson([
+            'result' => RESPONSE_FAIL,
+            'message' => '리스트 조회에 실패하였습니다.',
+        ]);
+    }
 });
 
 // URL 변환 및 저장
 $app->post('/shortener', function (Request $request, Response $response, array $args) {
     try {
+        $_title = $request->getParsedBodyParam('title');
+        $_longUrl = $request->getParsedBodyParam('longUrl');
+
+        if (empty($_title) || empty($_longUrl)) {
+            throw new Exception('요청값이 올바르지 않습니다.');
+        }
+
         $client    = new \GuzzleHttp\Client();
         $_response = $client->request('GET', 'https://api-ssl.bitly.com/v3/shorten', [
             'query'  => [
-                'longUrl'      => 'https://frend.tistory.com/12?category=711253',
+                'longUrl'      => $_longUrl,
                 'access_token' => BITLY_ACCESS_TOKEN,
             ],
             'verify' => false,
         ]);
 
-        if ($_response->getStatusCode() == 200) {
-            $body     = $_response->getBody();
-            $arr_body = json_decode($body);
-
-            return $response->withJson([
-                'data' => [
-                    'short' => $arr_body->data->url
-                ]
-            ]);
+        if ($_response->getStatusCode() !== 200) {
+            throw new Exception();
         }
+
+        $body     = $_response->getBody();
+        $arr_body = json_decode($body);
+
+        return $response->withJson([
+            'result' => RESPONSE_SUCCESS,
+            'message' => '정상적으로 등록되었습니다.',
+            'data' => [
+                'longUrl' => $_longUrl,
+                'shortUrl' =>   $arr_body->data->url,
+            ],
+        ]);
+
     } catch (Exception $e) {
-        $this->logger->info($e->getMessage());
+        $this->logger->error($e->getMessage());
+        return $response->withJson([
+            'result' => RESPONSE_FAIL,
+            'message' => 'URL 저장이 실패하였습니다.',
+        ]);
     }
 });
 
 // 삭제
-$app->delete('/shortener/{}', function (Request $request, Response $response, array $args) {
+$app->delete('/shortener/[{id}]', function (Request $request, Response $response, array $args) {
+    try {
+        $sth = $this->db->prepare("DELETE FROM shortener WHERE id=:id");
+        $sth->bindParam("id", $args['id']);
+        $sth->execute();
 
+        if ($sth->rowCount() === 0) {
+            throw new Exception('NOT_EXIST_ID');
+        }
+
+        return $response->withJson([
+            'result' => RESPONSE_SUCCESS,
+            'message' => '정상적으로 삭제되었습니다.',
+            'data'   => [
+                'id' => $args['id']
+            ]
+        ]);
+    } catch (Exception $e) {
+        $this->logger->error($e->getMessage());
+        return $response->withJson([
+            'result' => RESPONSE_FAIL,
+            'message' => '존재하지 않는 ID입니다.',
+        ]);
+    }
 });
--- server/src/settings.php
+++ server/src/settings.php
@@ -1,27 +1,30 @@
 <?php
 return [
     'settings' => [
-        'displayErrorDetails'    => true, // set to false in production
-        'addContentLengthHeader' => false, // Allow the web server to send the content-length header
+        'displayErrorDetails'               => true,
+        // set to false in production
+        'addContentLengthHeader'            => false,
+        // Allow the web server to send the content-length header
 
         // Renderer settings
-        'renderer'               => [
+        'renderer'                          => [
             'template_path' => __DIR__ . '/../templates/',
         ],
 
         // Monolog settings
-        'logger'                 => [
+        'logger'                            => [
             'name'  => 'slim-app',
-            'path'  => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
+            'path'  => isset($_ENV['docker']) ? 'php://stdout'
+                : __DIR__ . '/../logs/app.log',
             'level' => \Monolog\Logger::DEBUG,
         ],
 
         // mysql 셋팅
-        "db"                     => [
+        "db"                                => [
             "host"   => "localhost",
             "dbname" => "wodn4131",
             "user"   => "wodn4131",
             "pass"   => "wogud00."
         ],
-    ],
+    ]
 ];
